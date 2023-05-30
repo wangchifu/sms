e23ae93d8c52889cce2e75d35a5ee14db768b7a0
wangchifu <wangchifu@gmail.com>
2023-05-30 22:29:50 +0800

    fix search select js

--- resources/views/lunch_specials/teacher_change_month.blade.php
+++ resources/views/lunch_specials/teacher_change_month.blade.php
@@ -13,6 +13,10 @@
 @endsection
 
 @section('content')
+    <!-- Chosen v1.8.2 -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/chosen/1.8.2/chosen.min.css" rel="stylesheet" />
+    <link href="{{ asset('css/component-chosen.min.css') }}" rel="stylesheet" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/chosen/1.8.2/chosen.jquery.min.js"></script>
 <?php
 
 $active['teacher'] ="";
@@ -38,7 +42,12 @@ $active['setup'] ="";
                         <label>
                             選擇教職員
                         </label>
-                        {{ Form::select('user_id', $user_array,null, ['class' => 'form-control','placeholder'=>'--請選擇--','required'=>'required']) }}
+                        {{ Form::select('user_id', $user_array,null, ['class' => 'form-control search_selet','placeholder'=>'--請選擇--','required'=>'required']) }}
+                        <script>
+                            $( ".search_selet" ).chosen({
+                                search_contains: true,
+                            });
+                        </script>
                     </div>
                     <div class="form-group">
                         <label>
