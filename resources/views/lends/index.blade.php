af25706b55411eb141bd57981cc59dad1a7ff92f
wangchifu <wangchifu@gmail.com>
2024-03-12 09:59:24 +0800

    fix

--- resources/views/lends/index.blade.php
+++ resources/views/lends/index.blade.php
@@ -80,7 +80,7 @@ $active['list'] ="";
                                 <?php if(!isset($all_lend_num[$lend_item->id])) $all_lend_num[$lend_item->id]=0; ?>
                                 <tr style="background:#E0E0E0;font-weight:bold;color:black">
                                     <td>{{ $lend_item->name }}</td>
-                                    <td>{{ $lend_item->num-$all_lend_num[$lend_item->id] }} / {{ $lend_item->num }}</td>
+                                    <td>剩 {{ $lend_item->num-$all_lend_num[$lend_item->id] }} / {{ $lend_item->num }}</td>
                                     <td colspan="4">
                                     @if($lend_item->ps)
                                     <small class="text-primary">
